613143b3dbdcec7713996ae7b6a9457d0781f042
Diego-Alexander-udec <3132006817alexander@gmail.com>
2025-08-30 18:04:31 -0500

    Se añaede proyecto con su estructura

---
+++ Proyecto/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    name = None
+    name = "Flask"
+    return f"Hello, {name}!"
+
+   
+@app.route('/index')
+def index():
+    Myname= "Flask"
+    return render_template('index.html', name=Myname)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
